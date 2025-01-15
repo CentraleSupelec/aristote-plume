6b4f98dace68474483e8a103096397449e514f03
Matthieu Charreire <matthieu.charreire@illuin.tech>
2025-01-15 16:30:26 +0100

    fix: add server scheme parameter

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -9,6 +9,11 @@ from src.worker import celery_app, generate_article
 fastapi_app = FastAPI()
 
 
+@fastapi_app.get("/health")
+def get_task_status() -> str:
+    return "OK"
+
+
 @fastapi_app.post("/generate-article", response_model=ArticleCreationTaskDto)
 def start_task(article_request: ArticleRequest) -> ArticleCreationTaskDto:
     generation_task = generate_article.delay(article_request.model_dump())
