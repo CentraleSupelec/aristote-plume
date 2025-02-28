7ac816f19710e4782e69373b2ecac776bef44628
Matthieu Charreire <matthieu.charreire@illuin.tech>
2025-02-28 16:59:50 +0100

    feat: possibility to auto create user accounts

--- webapp/src/Security/PlumeUser/PlumeUserProvider.php
+++ webapp/src/Security/PlumeUser/PlumeUserProvider.php
@@ -4,17 +4,23 @@ namespace App\Security\PlumeUser;
 
 use App\Entity\PlumeUser;
 use App\Repository\PlumeUserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Drenso\OidcBundle\Exception\OidcException;
 use Drenso\OidcBundle\Model\OidcUserData;
 use Drenso\OidcBundle\Security\UserProvider\OidcUserProviderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 readonly class PlumeUserProvider implements OidcUserProviderInterface
 {
-    public function __construct(private PlumeUserRepository $plumeUserRepository)
-    {
+    public function __construct(
+        private PlumeUserRepository $plumeUserRepository,
+        private EntityManagerInterface $entityManager,
+        private ValidatorInterface $validator,
+        private bool $autoCreateAccount = false,
+    ) {
     }
 
     public function refreshUser(UserInterface $user): UserInterface
@@ -41,7 +47,19 @@ readonly class PlumeUserProvider implements OidcUserProviderInterface
         $plumeUser = $this->plumeUserRepository->findOneBy(['email' => $identifier]);
 
         if (!$plumeUser instanceof PlumeUser) {
-            throw new UserNotFoundException(sprintf('[PlumeUser] Username "%s" does not exist.', $identifier));
+            if ($this->autoCreateAccount) {
+                $plumeUser = (new PlumeUser())->setEmail($identifier)->setEnabled(true);
+
+                $errors = $this->validator->validate($plumeUser);
+                if (count($errors) > 0) {
+                    throw new UserNotFoundException(sprintf('[PlumeUser] User with email "%s" does not exist and could not be created.', $identifier));
+                }
+
+                $this->entityManager->persist($plumeUser);
+                $this->entityManager->flush();
+            } else {
+                throw new UserNotFoundException(sprintf('[PlumeUser] Username "%s" does not exist.', $identifier));
+            }
         }
 
         return $plumeUser;
